f13f26ef80200f90db2af50c8cb068657d4f42e0
Alektas <kriowolf1@gmail.com>
2019-10-15 15:41:34 +0300

    feature: add CDMA encoding

--- app/src/main/java/alektas/telecomapp/domain/entities/coders/CdmaCoder.kt
+++ app/src/main/java/alektas/telecomapp/domain/entities/coders/CdmaCoder.kt
@@ -12,10 +12,7 @@ class CdmaCoder : Coder<Array<Boolean>> {
      * @return двоичное число. Если код равен 0, то возвращается исходная информация.
      * Если информация отсутствует, то возвращается 0.
      */
-    override fun encode(
-        code: Array<Boolean>,
-        data: Array<Boolean>
-    ): Array<Boolean> {
+    override fun encode(code: Array<Boolean>, data: Array<Boolean>): Array<Boolean> {
         if (code.isEmpty() || data.isEmpty()) return data
 
         val spreadData = mutableListOf<Boolean>()
@@ -24,8 +21,16 @@ class CdmaCoder : Coder<Array<Boolean>> {
         return spreadData.toTypedArray()
     }
 
-    override fun decode(code: Array<Boolean>): Array<Boolean> {
-        return arrayOf()
+    override fun decode(code: Array<Boolean>, codedData: Array<Boolean>): Array<Boolean> {
+        return codedData.asList()
+            .chunked(code.size) {
+                average(it.mapIndexed { i, bit -> bit.xor(code[i]) })
+            }
+            .toTypedArray()
+    }
+
+    private fun average(data: List<Boolean>): Boolean {
+        return data.count { it }.let { it > data.size - it }
     }
 
 }
