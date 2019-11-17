722bddd9934ca8cc6a4e6a6cf136ecf31437a6ce
Alektas <kriowolf1@gmail.com>
2019-11-17 17:11:53 +0300

    feature: save last used by user demodulator filter settings

--- app/src/main/java/alektas/telecomapp/di/PreferencesModule.kt
+++ app/src/main/java/alektas/telecomapp/di/PreferencesModule.kt
@@ -3,6 +3,7 @@ package alektas.telecomapp.di
 import alektas.telecomapp.R
 import alektas.telecomapp.domain.entities.QpskContract
 import alektas.telecomapp.domain.entities.demodulators.DemodulatorConfig
+import alektas.telecomapp.domain.entities.filters.FilterConfig
 import android.content.Context
 import android.content.SharedPreferences
 import dagger.Module
@@ -33,12 +34,44 @@ class PreferencesModule {
     @Provides
     fun providesDemodulatorConfig(
         context: Context,
-        @Named("demodulatorPrefs") demodulatorPrefs: SharedPreferences
+        @Named("demodulatorPrefs") demodulatorPrefs: SharedPreferences,
+        filterConfig: FilterConfig
     ): DemodulatorConfig {
         val genFreq = demodulatorPrefs.getFloat(
             context.getString(R.string.demodulator_generator_freq_key),
             QpskContract.DEFAULT_CARRIER_FREQUENCY.toFloat()
         ).let { it * 1.0e6 }
-        return DemodulatorConfig(carrierFrequency = genFreq)
+        return DemodulatorConfig(
+            carrierFrequency = genFreq,
+            filterConfig = filterConfig
+        )
+    }
+
+
+    @Provides
+    fun providesDemodulatorFilterConfig(
+        context: Context,
+        @Named("demodulatorPrefs") demodulatorPrefs: SharedPreferences
+    ): FilterConfig {
+        val order = demodulatorPrefs.getInt(
+            context.getString(R.string.demodulator_filter_order_key),
+            FilterConfig.DEFAULT_ORDER
+        )
+
+        val cutoffFreq = demodulatorPrefs.getFloat(
+            context.getString(R.string.demodulator_filter_cutoff_freq_key),
+            FilterConfig.DEFAULT_BANDWIDTH.toFloat()
+        ).toDouble()
+
+        val windowType = demodulatorPrefs.getInt(
+            context.getString(R.string.demodulator_filter_window_type_key),
+            FilterConfig.DEFAULT_WINDOW_TYPE
+        )
+
+        return FilterConfig(
+            order = order,
+            bandwidth = cutoffFreq,
+            windowType = windowType
+        )
     }
 }
