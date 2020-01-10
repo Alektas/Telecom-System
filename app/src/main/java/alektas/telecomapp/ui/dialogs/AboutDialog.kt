8cdfe20beee41b34212381d2f17dc10e10348b61
Alektas <kriowolf1@gmail.com>
2020-01-10 20:34:18 +0300

    fix: version name in the about app page doesn't show

--- app/src/main/java/alektas/telecomapp/ui/dialogs/AboutDialog.kt
+++ app/src/main/java/alektas/telecomapp/ui/dialogs/AboutDialog.kt
@@ -4,18 +4,22 @@ import alektas.telecomapp.BuildConfig
 import alektas.telecomapp.R
 import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 
 class AboutDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return AlertDialog.Builder(requireContext()).apply {
-            setView(R.layout.dialog_about)
-            view?.findViewById<TextView>(R.id.app_version)?.text = BuildConfig.VERSION_NAME
-            setTitle(getString(R.string.menu_about))
-            setNegativeButton(getString(R.string.cancel)) { _, _ -> dismiss() }
-        }
+        val view = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_about, null)
+        view.findViewById<TextView>(R.id.app_version)?.text = BuildConfig.VERSION_NAME
+
+        return AlertDialog.Builder(requireContext())
+            .apply {
+                setView(view)
+                setTitle(getString(R.string.menu_about))
+                setNegativeButton(getString(R.string.cancel)) { _, _ -> dismiss() }
+            }
             .create()
     }
 }
