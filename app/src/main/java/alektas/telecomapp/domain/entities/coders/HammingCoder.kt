1c8153da1e794df81307f68984e511b435772b33
Alektas <kriowolf1@gmail.com>
2020-02-01 20:46:06 +0300

    feature: Hamming coder with determined code word size

--- app/src/main/java/alektas/telecomapp/domain/entities/coders/HammingCoder.kt
+++ app/src/main/java/alektas/telecomapp/domain/entities/coders/HammingCoder.kt
@@ -1,126 +1,179 @@
 package alektas.telecomapp.domain.entities.coders
 
-import alektas.telecomapp.utils.isPowerOfTwo
-import androidx.annotation.VisibleForTesting
-import kotlin.math.pow
+/**
+ * Coder that transforms information data bits to the Hamming code and vice versa.
+ * Hamming codes allows to fix one error bit in code word.
+ * If the data is larger than the code word can contain, then it will be divided to the several words.
+ *
+ * @param wordSize size of the code words. Warning! It must be greater than 2!
+ */
+class HammingCoder(val wordSize: Int) {
+    val parityBitsInWord = getParityBitsCount(wordSize)
+    val dataBitsInWord = wordSize - parityBitsInWord
+    /**
+     * Characteristic of the coder that display fraction of extra parity bits in the each word.
+     */
+    val redundancy = parityBitsInWord / wordSize.toFloat()
+    /**
+     * Characteristic of the coder that display fraction of information bits in the each word.
+     */
+    val rate = dataBitsInWord / wordSize.toFloat()
+    private val parityIndices = IntArray(parityBitsInWord) { 1.shl(it) - 1 }
 
-class HammingCoder {
+    /**
+     * Encode data by Hamming code.
+     * It adds extra parity bits, so output data is larger than input.
+     * If the data is not a multiple of [wordSize], extra zero bits adds to the end of the data
+     * (to find out how much use [getExtraBitsCount] method).
+     *
+     * @param data arbitrary number of information bits
+     * @return coded data (concatenation of the code words)
+     */
+    fun encode(data: BooleanArray): BooleanArray = data
+        .toRowCodeWords()
+        .map { encode(it).bits }
+        .toFlatData()
 
-    fun encode(data: BooleanArray): BooleanArray {
-        val codedData = data.spread()
-        val parityBits = calcParityBits(codedData)
-        codedData.fillBy(parityBits)
-        return codedData
+    /**
+     * Decode coded data (concatenation of the code words) by Hamming code.
+     *
+     * @param codedData concatenation of the code words' bits
+     * @return information bits, extracted from the coded data
+     */
+    fun decode(codedData: BooleanArray): BooleanArray = codedData
+        .asCodeWords()
+        .map { decode(it) }
+        .toFlatData()
+
+    /**
+     * Calculate extra bits count, that will be added to the end of the data while encoding
+     * to fill the last code word.
+     *
+     * @param dataSize bits count of the data that will be or were encoded
+     * @return extra bits count
+     */
+    fun getExtraBitsCount(dataSize: Int): Int {
+        val reminder = dataSize % dataBitsInWord
+        if (reminder == 0) return 0
+        return dataBitsInWord - reminder
     }
 
-    fun decode(codedData: BooleanArray): BooleanArray {
-        val calculatedParityBits = calcParityBits(codedData)
-        val invalidIndex = getInvalidBitIndex(calculatedParityBits)
-        return codedData.extractValidData(invalidIndex)
+    private fun encode(word: CodeWord): CodeWord {
+        val parityBits = calculateParityBits(word)
+        return word.with(parityBits)
     }
 
-    private fun BooleanArray.spread(): BooleanArray {
-        val codedData = mutableListOf<Boolean>()
-        var dataIndex = 0
-        var counter = 1
-        while (dataIndex != this.size) {
-            if (counter.isPowerOfTwo()) {
-                codedData.add(false)
-            } else {
-                codedData.add(this[dataIndex])
-                dataIndex++
-            }
-            counter++
-        }
-        return codedData.toBooleanArray()
+    private fun decode(word: CodeWord): BooleanArray {
+        val parityBits = calculateParityBits(word)
+        val invalidIndex = parityBits.toInvalidBitIndex()
+        return word.extractValidData(invalidIndex)
     }
 
     /**
      * @return For encoding this method return parity bits.
      * For decoding returns array of zeros if there are no errors in the block. If there is
      * a error in the block, then returns inverted bits of the error position. To translate
-     * the result to the error bit index use [getInvalidBitIndex].
+     * the result to the error bit index use [ParityBits.toInvalidBitIndex].
      */
-    @VisibleForTesting
-    fun calcParityBits(block: BooleanArray): BooleanArray {
-        val parityBitsCount = parityBitsCount(block.size)
-        val parityBits = BooleanArray(parityBitsCount)
-        for (i in block.indices) {
-            if (!block[i]) continue // count only '1' bits
+    private fun calculateParityBits(word: CodeWord): ParityBits {
+        val bits = BooleanArray(parityBitsInWord)
+        for (i in word.indices) {
+            if (!word[i]) continue // count only '1' bits
             var position = i + 1
             var k = 0 // parity bits array pointer
             while (position != 0) {
                 // count only for the corresponding parity bits
                 if (Integer.lowestOneBit(position) == 1) {
-                    parityBits[k] = parityBits[k].xor(block[i])
+                    bits[k] = bits[k].xor(word[i])
                 }
                 k++
                 position = position.ushr(1)
             }
         }
-        return parityBits
+        return ParityBits(bits)
     }
 
-    private fun BooleanArray.fillBy(parityBits: BooleanArray) {
-        parityBits.forEachIndexed { p, b ->
-            val position = 2.0.pow(p).toInt()
-            this[position - 1] = b
-        }
-    }
-
-    @VisibleForTesting
-    fun getParityBits(block: BooleanArray): BooleanArray {
-        val size = parityBitsCount(block.size)
-        val parityBits = BooleanArray(size)
-        var blockPointer = 1
-        var i = 0
-        while (blockPointer < block.size) {
-            parityBits[i] = block[blockPointer - 1]
-            i++
-            blockPointer = blockPointer.shl(1)
-        }
-        return parityBits
-    }
-
-    private fun BooleanArray.extractValidData(invalidIndex: Int): BooleanArray {
-        val data = mutableListOf<Boolean>()
-        this.forEachIndexed { i, b ->
-            if (!(i + 1).isPowerOfTwo()) {
-                data.add(if (i == invalidIndex) b.not() else b)
-            }
-        }
-        return data.toBooleanArray()
-    }
-
-    private fun getInvalidBitIndex(
-        calculatedParityBits: BooleanArray,
-        receivedParityBits: BooleanArray
-    ): Int {
-        return calculatedParityBits.foldIndexed(0) { i, acc: Int, b ->
-            if (b != receivedParityBits[i]) acc + i else acc
-        }
-    }
-
-    /**
-     * Translate parity bits to the error bit index in the data block.
-     *
-     * @return error bit index. If there is no errors, returns -1
-     */
-    private fun getInvalidBitIndex(parityBits: BooleanArray): Int {
-        val invalidPosition = parityBits.foldRight(0) { b, acc ->
-            acc.shl(1) + if (b) 1 else 0
-        }
-        return invalidPosition - 1
-    }
-
-    @VisibleForTesting
-    fun parityBitsCount(blockSize: Int): Int {
-        var x = blockSize
+    private fun getParityBitsCount(wordSize: Int): Int {
+        var x = wordSize
         var count = 0
         while (x != 0) {
             count++
             x = x.ushr(1)
         }
         return count
+    }
+
+    private fun List<BooleanArray>.toFlatData(): BooleanArray =
+        this.foldIndexed(BooleanArray(this.size * this[0].size)) { i, acc, block ->
+            block.copyInto(acc, i * this[0].size)
+        }
+
+    /**
+     * Divide data flow to the code words (each of [wordSize] length) with empty parity bits
+     */
+    private fun BooleanArray.toRowCodeWords(): List<CodeWord> =
+        this.toList().chunked(dataBitsInWord) { it.toRowCodeWord() }
+
+    /**
+     * Convert list of data bits to the code word with empty parity bits. If the data is lesser than
+     * the code word, adds extra zero bits to fill the word.
+     */
+    private fun List<Boolean>.toRowCodeWord(): CodeWord {
+        var pointer = 0 // pointer for the data array
+        val data = BooleanArray(wordSize)
+        for (i in data.indices) {
+            if (parityIndices.contains(i) || pointer >= this.size) {
+                data[i] = false
+                continue
+            }
+            data[i] = this[pointer]
+            pointer++
+        }
+        return CodeWord(data)
+    }
+
+    private fun BooleanArray.asCodeWords(): List<CodeWord> =
+        this.asList().chunked(wordSize) { CodeWord(it.toBooleanArray()) }
+
+    inner class CodeWord(val bits: BooleanArray) {
+        val size = bits.size
+        val indices = bits.indices
+
+        operator fun get(index: Int): Boolean = bits[index]
+
+        fun with(parityBits: ParityBits): CodeWord {
+            parityBits.bits.forEachIndexed { p, b ->
+                this.bits[1.shl(p) - 1] = b
+            }
+            return this
+        }
+
+        fun extractValidData(invalidIndex: Int): BooleanArray {
+            val data = mutableListOf<Boolean>()
+            bits.forEachIndexed { i, b ->
+                if (!parityIndices.contains(i)) {
+                    data.add(if (i == invalidIndex) b.not() else b)
+                }
+            }
+            return data.toBooleanArray()
+        }
+    }
+
+    inner class ParityBits(val bits: BooleanArray) {
+        val size = bits.size
+
+        operator fun get(index: Int): Boolean = bits[index]
+
+        /**
+         * Translate parity bits to the error bit index in the data block.
+         *
+         * @return error bit index. If there is no errors, returns -1
+         */
+        fun toInvalidBitIndex(): Int {
+            val invalidPosition = bits.foldRight(0) { b, acc ->
+                acc.shl(1) + if (b) 1 else 0
+            }
+            return invalidPosition - 1
+        }
     }
 }
--- app/src/test/java/alektas/telecomapp/domain/entities/coders/HammingCoderTest.kt
+++ app/src/test/java/alektas/telecomapp/domain/entities/coders/HammingCoderTest.kt
@@ -4,11 +4,11 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class HammingCoderTest {
-    val coder = HammingCoder()
+    val coder7 = HammingCoder(7)
 
     @Test
     fun encode_1011_is0110011() {
-        val actual = coder.encode(booleanArrayOf(true, false, true, true))
+        val actual = coder7.encode(booleanArrayOf(true, false, true, true))
         val expected = booleanArrayOf(false, true, true, false, false, true, true)
 
         println("actual=${actual.contentToString()}")
@@ -18,7 +18,7 @@ class HammingCoderTest {
 
     @Test
     fun decode_0110011_is1011() {
-        val actual = coder.decode(booleanArrayOf(false, true, true, false, false, true, true))
+        val actual = coder7.decode(booleanArrayOf(false, true, true, false, false, true, true))
         val expected = booleanArrayOf(true, false, true, true)
 
         println("actual=${actual.contentToString()}")
@@ -27,64 +27,18 @@ class HammingCoderTest {
     }
 
     @Test
-    fun getParityBits_0110011_is010() {
-        val actual = coder.getParityBits(booleanArrayOf(false, true, true, false, false, true, true))
-        val expected = booleanArrayOf(false, true, false)
-
-        println("actual=${actual.contentToString()}")
-        println("expected=${expected.contentToString()}")
-        assertArrayEquals(expected, actual)
-    }
-
-
-    @Test
-    fun getParityBits_111_is11() {
-        val actual = coder.getParityBits(booleanArrayOf(true, true, true))
-        val expected = booleanArrayOf(true, true)
-
-        println("actual=${actual.contentToString()}")
-        println("expected=${expected.contentToString()}")
-        assertArrayEquals(expected, actual)
+    fun getExtraBitsCount_whenDataSizeIs11_is1() {
+        assertEquals(1, coder7.getExtraBitsCount(11))
     }
 
     @Test
-    fun calcParityBits_0110011_is010() {
-        val actual = coder.calcParityBits(booleanArrayOf(false, true, true, false, false, true, true))
-        val expected = booleanArrayOf(false, false, false)
-
-        println("actual=${actual.contentToString()}")
-        println("expected=${expected.contentToString()}")
-        assertArrayEquals(expected, actual)
+    fun getExtraBitsCount_whenDataSizeIs3_is1() {
+        assertEquals(1, coder7.getExtraBitsCount(3))
     }
 
     @Test
-    fun parityBitsCount_of16_is5() {
-        assertEquals(5, coder.parityBitsCount(16))
-    }
-
-    @Test
-    fun parityBitsCount_of15_is4() {
-        assertEquals(4, coder.parityBitsCount(15))
-    }
-
-    @Test
-    fun parityBitsCount_of8_is4() {
-        assertEquals(4, coder.parityBitsCount(8))
-    }
-
-    @Test
-    fun parityBitsCount_of7_is3() {
-        assertEquals(3, coder.parityBitsCount(7))
-    }
-
-    @Test
-    fun parityBitsCount_of4_is3() {
-        assertEquals(3, coder.parityBitsCount(4))
-    }
-
-    @Test
-    fun parityBitsCount_of3_is2() {
-        assertEquals(2, coder.parityBitsCount(3))
+    fun getExtraBitsCount_whenDataSizeIs8_is0() {
+        assertEquals(0, coder7.getExtraBitsCount(8))
     }
 
 }
